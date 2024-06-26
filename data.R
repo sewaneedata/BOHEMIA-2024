8578f7a44ea4e4408412aee0bf54de1b2bb7e781
Oysik Saif <oysik10@Oysiks-MacBook-Pro.local>
2024-06-26 15:47:24 -0500

    Creating new dataset and keeping specific columns

--- data.R
+++ data.R
@@ -14,7 +14,7 @@ kenya_safety<- read_csv('dataset/kenya_safety.csv')
 
 #creating new dataset with columns required from safety
 kenya_nsafety<-kenya_safety %>% 
-  mutate('type'='og') %>% 
+  mutate('type'='og') %>% #type of dataset (safety or new)
   select(extid, visit, type, todays_date,
          sleep_net_last_night, nights_sleep_net, ae_symptoms, 
          pregnant_yn,
@@ -23,10 +23,10 @@ kenya_nsafety<-kenya_safety %>%
          anc, iptp)
 
 #creating new dataset with columns required from safetynew 
-kenya_nsafetynew<-kenya_safetynew %>% 
-  mutate('type'='new') %>% 
-  mutate(ae_symptoms=NA) %>% 
-  mutate(months_pregnant_dk=NA) %>% 
+kenya_nsafetynew<-kenya_safetynew %>%
+  mutate('type'='new') %>% #type of dataset (safety or new) 
+  mutate(ae_symptoms=NA) %>%  #ae_symptoms not present in the new one
+  mutate(months_pregnant_dk=NA) %>% #months_pregnant_dk not present in the new one
   select(extid, visit, type, todays_date, 
          sleep_net_last_night, nights_sleep_net, 
          ae_symptoms, 
@@ -48,7 +48,7 @@ kenya_safety_total<- rbind(kenya_nsafety, kenya_nsafetynew)  %>%
 
 
 kenya_nhealthecon<-kenya_healthecon_baseline %>% 
-  mutate(type='og') %>% 
+  mutate(type='og') %>% #type of dataset (baseline v new)
   select(extid, #
          type, 
          todays_date,  
@@ -104,10 +104,10 @@ kenya_nhealtheconnew <- kenya_healthecon_baseline_new %>%
   rowwise() %>%
   mutate(num_bed_nets = ifelse(hhid %in% kenya_healthecon_baseline$hhid, 
                                kenya_healthecon_baseline$num_bed_nets[which(kenya_healthecon_baseline$hhid == hhid)], 
-                               num_bed_nets)) %>%
+                               num_bed_nets)) %>% #adding num_bed_nets as it is not present in new dataset
   ungroup() %>% 
   mutate(type='new') %>% 
-  mutate(malaria_care_where_specify=NA) %>% 
+  mutate(malaria_care_where_specify=NA) %>% #putting NA to columns that are not present in the new dataset
   mutate(num_visit_hospital=NA) %>% 
   mutate(num_visit_pharmacy=NA) %>% 
   mutate(num_visit_informal_drug_vendor=NA) %>% 
@@ -177,8 +177,79 @@ kenya_nhealtheconnew <- kenya_healthecon_baseline_new %>%
          seek_care_other_ill_malaria_treatment_kes,
          seek_care_other_ill_other_kes)
 
+#binding healthecon with healtheconnew and then joining demography
 kenya_healthecon_total<-rbind(kenya_nhealthecon, kenya_nhealtheconnew) %>% 
   left_join(kenya_ndemography, by='extid')
 
+#selecting columns from efficacy and then joining demography
+kenya_efficacy_total<-kenya_efficacy %>% 
+  select(extid,
+         start_time,
+         end_time,
+         visits_done, 
+         malaria_fever_last_month,
+         treated_for_malaria_yn,
+         malaria_treatment,
+         sleep_under_net_last_night,
+         num_nights_sleep_under_net
+  ) %>% 
+  left_join(kenya_ndemography, by='extid')
 
-
+#selecting columns from healtheconmonthly and adding demography
+healtheconmonthly_total <- healthecon_monthly %>%
+  rowwise() %>%
+  mutate(num_bed_nets = ifelse(hhid %in% kenya_healthecon_baseline$hhid, 
+                               kenya_healthecon_baseline$num_bed_nets[which(kenya_healthecon_baseline$hhid == hhid)], 
+                               num_bed_nets)) %>% #adding num_bed_nets from baseline
+  select(extid, #
+         todays_date,  
+         num_bed_nets, extid, #
+         todays_date,  
+         num_bed_nets, 
+         malaria_care_yn,
+         malaria_care_where,
+         malaria_care_where_specify,
+         num_visit_hf,
+         num_visit_hospital,
+         num_visit_pharmacy,
+         num_visit_informal_drug_vendor,
+         num_visit_other,
+         overnight_hospital,
+         num_overnight_hospital,
+         malaria_test_yn,
+         malaria_test_result,
+         malaria_treatment_yn,
+         malaria_consult_tests_kes,
+         malaria_hospitalization_kes,
+         malaria_medication_kes,
+         malaria_travel_to_clinic_kes,
+         malaria_food_kes,
+         malaria_other_kes,
+         malaria_miss_school,
+         num_malaria_miss_school_days,
+         malaria_miss_work,
+         num_malaria_miss_work_days,
+         malaria_away_pay,
+         malaria_away_pay_kes,
+         other_member_care,
+         other_member_care_select,
+         seek_care_other_ill,
+         seek_care_other_ill_where,
+         seek_care_other_ill_where_specify,
+         num_visit_hf_other_ill,
+         num_visit_hospital_other_ill,
+         num_visit_pharmacy_other_ill,
+         num_visit_informal_drug_vendor_other_ill,
+         num_visit_traditional_healer_other_ill,
+         num_visit_other_other_ill,
+         other_ill_overnight_hopsital,
+         num_nights_other_ill_hopsital,
+         seek_care_other_ill_malaria_test,
+         seek_care_other_ill_malaria_test_result,
+         seek_care_other_ill_malaria_treatment,
+         seek_care_other_ill_malaria_test_kes,
+         seek_care_other_ill_malaria_treatment_kes,
+         seek_care_other_ill_other_kes) %>% 
+  left_join(kenya_ndemography, by='extid')
+  
+  
