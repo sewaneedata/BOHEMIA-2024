bf32b7c25c37cf0e32b3057e1493b107e4b31838
Loaner <loaner@StudentLoaner3278.local>
2024-07-01 15:06:33 -0500

    making the R script for the second question

---
+++ correlations.R
@@ -0,0 +1,2 @@
+# PURPOSE:
+# Creating a bar chart,  showing OSU over seven visits for efficacy dataset and over 4 visits for safety data set. Additionally, creating a tabulated format of five number summary of sufficiency
