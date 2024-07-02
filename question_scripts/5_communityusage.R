eb065450dcef24ca48d15d35610f2cab06256b18
Samuel Grace-Conyers <sgraceconyers@gmail.com>
2024-07-02 13:27:44 -0500

    progress on q5

--- question_scripts/5_communityusage.R
+++ question_scripts/5_communityusage.R
@@ -1,1 +1,12 @@
 #PURPOSE:Scatterplot of efficacy data where the x-axis is malaria incidents and y-axis is percent usage and coloured by villages. 
+
+#load data
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+kenya_efficacy_table<-kenya_efficacy_total
+
+kenya_efficacy_table %>% 
+  
+  
